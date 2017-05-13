a1367dd5d7eb3ee0bdf2c487a326f16e1ca50866
ParamasivamSivakumar <ParamasivamSivakumar>
2017-05-14 02:11:58 +0530

    AOF first commit, null pointer exception while running

---
+++ src/test/java/AOF/TestParallel.java
@@ -0,0 +1,28 @@
+package AOF;
+
+import java.net.MalformedURLException;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class TestParallel extends BaseTest {
+	@Test
+    public void test_01() throws InterruptedException, MalformedURLException{
+        try{
+            getDriver().get("http://www.w3schools.com/");
+ 
+            getDriver().findElement(By.xpath("html/body/div[2]/div/a[4]")).click();
+ 
+            //Wait intentially added to show parallelism execution
+            Thread.sleep(10000);
+ 
+            getDriver().findElement(By.xpath("//*[@id='gsc-i-id1']")).sendKeys("test");
+            Thread.sleep(5000);
+ 
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+}
